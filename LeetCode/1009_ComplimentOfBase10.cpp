e26d48a3f96358b5120116c3d9434e5ab3fa974c
Priyansh Bahoria <ipriyanshbahoria@gmail.com>
2026-09-03 20:08:30 +0530

    solved Question 1009

---
+++ LeetCode/1009_ComplimentOfBase10.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+using namespace std;
+
+int BitwiseComplement(int n){
+    int rem,ans=0,mul=1;
+    if (n==0){
+        return 1;
+    }
+    while(n){
+        rem=n%2;
+        rem=rem ^ 1;
+        n/=2;
+        ans+=rem*mul;
+        mul*=2;
+    }
+    return ans;
+
+}
+
+int main(){
+    int num;
+    cout<<"Enter a number: ";
+    cin>>num;
+    cout << BitwiseComplement(num);
+
+    return 0;
+}
