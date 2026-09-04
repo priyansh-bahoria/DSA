97c2fab928292aef8321be6a6945899ef606345c
Priyansh Bahoria <ipriyanshbahoria@gmail.com>
2026-09-04 12:57:17 +0530

    Update functions.cpp

--- basic/functions.cpp
+++ basic/functions.cpp
@@ -2,26 +2,44 @@
 #include <iostream>
 using namespace std;
 
-bool prime(int n){
+//bool prime (int n=3) now this has become a default parameter 
+
+bool prime(int n)
+{
     if(n<2) return 0;
+
     for(int i=2;i<n;i++){
         if (n%2==0) return 0;
     }
     return 1;
 }
 
-int factorial(int n){
-    int ans=1;
+int factorial(int n) // Function declare
+{ 
+    int ans=1;       // Function define
+
     for(int i=1;i<=n;i++){
         ans=ans*i;
     }
     return ans;    
 }
 
+//function with two parameters
+int sum(int x, int y)
+{
+    int ans=x+y;
+    return ans;
+}
+
 int main(){
-    int num;
+    int num,num1;
     cout<<"Enter a number: ";
     cin>>num;
     //cout << prime(num);
-    cout << factorial(num);
+    //cout << factorial(num);
+    
+
+    //cout<<"Enter a numbers: ";
+    //cin>>num>>num1;
+    //cout << sum(num,num1);
 }
