40b7f23749d7296b8d6dd5d4dba58d8abf26c93a
Priyansh Bahoria <ipriyanshbahoria@gmail.com>
2026-09-10 15:51:26 +0530

    Adding a Comment on Nim Game

--- LeetCode/292_NimGame.cpp
+++ LeetCode/292_NimGame.cpp
@@ -2,6 +2,7 @@
 #include <iostream>
 using namespace std;
 
+// Game of number, each player can only move 1-3 at a time and 
 bool canWinNim(int n){
     if(n%4==0) return false;
     else return true;
